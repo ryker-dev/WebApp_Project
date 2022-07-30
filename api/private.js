371eeeee555ffdbd4b1744c614f1c6c1822ef9bb
ryker-dev <pizzuti.dev@gmail.com>
2022-07-30 22:50:30 +0300

    removed file

--- api/private.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const validateToken = require("../auth/validateToken.js");
-
-router.get("/", validateToken, (req, res, next) => {
-    res.json({email: req.user.email});
-  });
-  
-module.exports = router;
